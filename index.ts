6e890cedfc3e1c6528258bff69fb55c75d8e0de2
Iuri Madeira <iurimadeira@Iuris-MacBook-Pro.local>
2020-09-20 12:51:28 -0300

    Adds docker-compose for mongodb; Connects app to mongodb using mongoose

--- index.ts
+++ index.ts
@@ -1,14 +1,48 @@
 import express from "express";
+import mongoose from "mongoose";
 const app = express();
 
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-  res.send("Hello world");
+  res.render("index", { name: "Iuri" });
 });
 
 app.get("/short", (req, res) => {
-  res.send("short url");
+  const db = mongoose.connection.db;
+  db.collection("test")
+    .insertOne({ fooBarTest: "completed" })
+    .then(
+      () => {
+        console.log("Created test document!");
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+
+  res.send({ ok: 1 });
 });
 
-app.listen(3000, () => {
-  console.log("Server started!");
+mongoose
+  .connect("mongodb://localhost", {
+    user: process.env.MONGODB_USER,
+    pass: process.env.MONGODB_PASS,
+    dbName: "url-shortener",
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(
+    (success) => {
+      console.log(`Connected to MongoDB (${success.connections[0].name})`);
+    },
+    (err) => {
+      console.log(err);
+    }
+  );
+
+mongoose.connection.on("open", async () => {
+  app.listen(process.env.PUBLIC_PORT, () => {
+    console.log("Server started!");
+  });
 });
